69f3af81527a13552366b47edc090a273785f01c
RepublicMarch <newker222@gmail.com>
2024-12-15 21:54:24 +0400

    CS

--- CSyntax/concepts.cpp
+++ CSyntax/concepts.cpp
@@ -1,27 +1,81 @@
 #include <iostream>
+#include <cstring>
 #include <concepts> // this is required to use concepts.
-    // concepts;
-    // concepts are much cleaner version of function or template.
-    // such as static casts.
 
-template <typename TypeOne>
-requires std::integral<TypeOne> // allows only integeal type on templates.
-// Syntax 2 requires std::integral<TupeOne>
-// Syntax 3 template <std::integral TypeOne>
-// Syntax 4 TypeOne (TypeOne a, TypeOne B) requires std::integeral<TypeOne>
+// concepts are much cleaner version of function or template.
+// such as static casts.
+
+// Call Template | template <typename TypeOne>
+// Syntax 1 | requires std::integral<TypeOne> | allows only integeal type on templates.
+// Syntax 2 | requires std::integral<TupeOne>
+// Syntax 3 | template <std::integral TypeOne>
+// Syntax 4 | TypeOne (TypeOne a, TypeOne B) requires std::integeral<TypeOne>
 
 // on function;
 // auto add (std::integral auto a, std::integral auto a)
 // check if parameter is a Integral type.
 
-TypeOne Addition (TypeOne A, TypeOne B)
+// Below shows different way to build concepts
+template <typename TypeTwo>
+concept MyIntegral = std::is_integral_v<TypeTwo>;
+
+template <typename TypeTwo>
+concept Multipliable = requires(TypeTwo a, TypeTwo b)
 {
-    int sum = A + B;
-    return sum;
+    a*b;
 };
+
+template <typename TypeTwo>
+concept Incrementable = requires(TypeTwo a)
+{
+    a+=1;
+    ++a;
+    a++;
+};
+
+// Using Custom Concepts we built above
+// - nothing special, they all do the same
+// SYNTAX 1
+template <typename TypeTwo>
+requires MyIntegral<TypeTwo> // << use trait we decleared from above
+TypeTwo Addition_One(TypeTwo A, TypeTwo B)
+{
+    return A+B;
+}
+// SYNTAX 2
+template <MyIntegral TypeTwo>
+TypeTwo Addition_Two(TypeTwo A, TypeTwo B)
+{
+    return A + B;
+}
+// SYNTAX 3
+auto Addition_Three(MyIntegral auto A, MyIntegral auto B)
+{
+    return A+B;
+}
+
+template <typename TypeTwo>
+requires Multipliable<TypeTwo>
+TypeTwo Multiple_One(TypeTwo A, TypeTwo B)
+{
+    return A * B;
+}
+
+template <typename TypeTwo>
+requires Incrementable<TypeTwo>
+TypeTwo Increment(TypeTwo A)
+{
+    return A;
+}
+
 
 int main()
 {
-    std::cout << Addition(24, 24) << std::endl;
+    std::string String_One {"Strings"};
+
+    std::cout << Addition_One(5,5) << std::endl;
+    std::cout << Multiple_One(5,5) << std::endl;
+    std::cout << Increment(5) << std::endl;
+    // Compile error. std::cout << Increment(String_One) << std::endl;
     return 0;
 }
---
+++ Computer_Science/Book_Data_Structures_And_Algorithm_Design_Principles_John/List_Stack_Queue.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <array> // to use std::array
+
+int main ()
+{
+    size_t Size_Notation {}; // integer value for null. it just for placeholder positive numb
+
+    // LINEAR DATA STRUCTURES
+
+    // Contiguous Data Structuers - O(1)
+    // Linked Data Structuers - O(n)
+
+    // Use CDS when Effective and Accurate Size
+    // Use LDS when Additional Space is expected
+
+    // Static Array - Will deleted when code bracket ends.
+    int Cpp_Array[]{2,3,4}; // align to stack memory
+
+    // Dynamic Array - Can set when will created and deleted
+    int* Dynamic_Cpp_Array = new int[Size_Notation]; // align to heap memory
+
+    // std::array - CDS Secure version of array
+    std::array<int, 10> Array_One; // std::array<DataType, Size> Name;
+    Array_One[0] = {1}; // Set Array First Space to 1
+    std::cout << "Accessing First Array Space : "<<Array_One[0] << std::endl;
+
+    std::array<int, 4> Array_Init_One = {1,2,3,4}; // Can be initialized and assign same time.
+
+    for (auto Elements : Array_Init_One)
+    {
+        std::cout << Elements << std::endl; // 1,2,3,4
+    }
+
+    // at(index) - Accurate but slower than [] Operator
+
+    try
+    {
+        std::cout << "Using at() : " <<Array_Init_One.at(3) << std::endl; // 4
+        std::cout << "Using at() : " << Array_Init_One.at(4) << std::endl; // Fatal Error - Crash;
+        std::cout << "Using [] : " << Array_Init_One[3] << std::endl; // no error
+        std::cout << "Using [] : " << Array_Init_One[4] << std::endl; // no error
+    }
+    catch (const std::out_of_range& ErrorMessage)
+    {
+        std::cout << ErrorMessage.what() << std::endl;
+    }
+
+    return 0;
+}
